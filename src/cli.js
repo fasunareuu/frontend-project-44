41491f77477b3cd7df4d0f22e0380ec93eb58511
fsnareu26 <yt.fasu@bk.ru>
2023-12-01 10:01:40 +0300

    task 1 - 8

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
-const getName = () => {
+// eslint-disable-next-line import/no-mutable-exports
+export let name = '';
 
-const name = readlineSync.question('May I have your name?: ');
-console.log(`Hello, ${name}!`);
-}
-
-export default getName;
+export const getName = () => {
+  name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}!`);
+};
